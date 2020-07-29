a2180af408f10412263c25311c4d1b1e77b6c0fd
seokyoungkimm <seokyoungkimm@gmail.com>
2020-07-29 17:08:09 +0900

    onDelete 추가 완료

--- src/containers/detailPost/DetailPostContainer.jsx
+++ src/containers/detailPost/DetailPostContainer.jsx
@@ -2,14 +2,21 @@ import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router';
 import DetailPost from '../../components/detailPost/DetailPost';
 
 const Wrapper = styled.div``;
 
 const DetailPostContainer = () => {
   const [data, setData] = useState('');
+  const history = useHistory();
 
   const { id } = useParams();
+
+  const onDelete = (e) => {
+    axios.delete(`http://localhost:8080/api/v1/study/${id}`)
+    history.push(`/`);
+  }
 
   const fetchData = useCallback(async () => {
     const { data } = await axios.get(`http://localhost:8080/api/v1/study/${id}`);
@@ -22,7 +29,10 @@ const DetailPostContainer = () => {
 
   return (
     <Wrapper>
-      <DetailPost data={data} id={id} />
+      <DetailPost 
+        data={data} 
+        id={id}
+        onDelete={onDelete} />
     </Wrapper>
   );
 };
