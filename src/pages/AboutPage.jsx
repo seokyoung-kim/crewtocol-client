5bf897b198fbd27a85eb207a3caffe40f24585aa
seokyoungkimm <seokyoungkimm@gmail.com>
2020-07-29 17:05:23 +0900

    About 페이지 추가

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import DetailPostContainer from '../containers/detailPost/DetailPostContainer';
 import Header from '../components/common/Header';
 import AboutPageContainer from '../containers/aboutPage/AboutPageContainer';
 
